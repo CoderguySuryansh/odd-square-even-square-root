9dc37138e6d42a7f07a920e359128b168a81f42f
CoderguySuryansh <98791574+CoderguySuryansh@users.noreply.github.com>
2022-03-09 10:36:12 +0530

    Add files via upload

---
+++ even square root & odd square.py
@@ -0,0 +1,10 @@
+x=float(input("ENTER A NUMBER :"))
+
+if x%2==0 :
+    c=x**0.5
+    print("ENTERED NUMBER IS EVEN AND ITS SQUARE ROOT :",c)
+
+else :
+    d=x*x
+    print("ENTERED NUMBER IS ODD AND ITS SQUARE :",d)
+    
